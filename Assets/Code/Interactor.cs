1e23ae5947f28e4abe5d0b9ae6fe68a48f3c919c
Towich <towichf@gmail.com>
2023-04-19 20:24:19 +0300

    misc changes

--- Assets/Code/Interactor.cs
+++ Assets/Code/Interactor.cs
@@ -3,16 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Interactor class for interaction with <<IInteractable>> objects
+/// </summary>
 public class Interactor : MonoBehaviour
 {
-    [SerializeField] private Transform interactionPoint;
-    [SerializeField] private float interactionPointRadius;
-    [SerializeField] private LayerMask interactableMask;
+    [SerializeField] private Transform interactionPoint; // center of interactor sphere
+    [SerializeField] private float interactionPointRadius; // radius of interactor sphere
+    [SerializeField] private LayerMask interactableMask; // mask of Interactable objects
 
-    private readonly Collider2D[] _colliders = new Collider2D[3];
-    [SerializeField] private int numFound;
+    private readonly Collider2D[] _colliders = new Collider2D[3]; // Interactable objects which interacts with sphere now
+    [SerializeField] private int numFound; // num of founded object which interacts with sphere
 
-    private IInteractable showingHintCollider;
+    private IInteractable showingHintCollider; // object which showing a hint now
 
     private void Start()
     {
@@ -21,14 +25,15 @@ public class Interactor : MonoBehaviour
 
     private void Update()
     {
+        // founding Interactable objects in sphere
         numFound = Physics2D.OverlapCircleNonAlloc(interactionPoint.position, interactionPointRadius, _colliders, interactableMask);
         
         
-        if (numFound > 0)
+        if (numFound > 0) // if Interactable object founded
             CheckFoundedCollider();
-        else if (showingHintCollider != null)
+        else if (showingHintCollider != null) // if showing hint now
         {
-            showingHintCollider.ShowHint(false);
+            showingHintCollider.ShowHint(false); // disable showing hint
             showingHintCollider = null;
         }
 
@@ -36,8 +41,10 @@ public class Interactor : MonoBehaviour
 
     private void CheckFoundedCollider()
     {
-        var interactable = _colliders[0].GetComponent<IInteractable>(); // getting component IInteractable of founded collider (for example: Chest/Door)
+        // getting component IInteractable of founded collider (for example: Chest/Door/DeadBody)
+        var interactable = _colliders[0].GetComponent<IInteractable>(); 
         
+        // if co
         if (interactable != null)
         {
             interactable.ShowHint(true);
