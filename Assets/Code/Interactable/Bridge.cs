08b973bffb1e9e7638a2789f75d0cf27c5495d66
Towich <towichf@gmail.com>
2023-04-22 16:26:19 +0300

    realized Bridge

--- Assets/Code/Interactable/Bridge.cs
+++ Assets/Code/Interactable/Bridge.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Bridge : MonoBehaviour, IInteractable
 {
@@ -19,15 +20,17 @@ public class Bridge : MonoBehaviour, IInteractable
 
     public bool Interact(Interactor interactor) {
         Inventory inv = interactor.GetComponent<Inventory>();
+        PlayerController pl = interactor.GetComponent<PlayerController>();
 
         if (inv) {
             if (inv.HasItem("Универсальный строительный инструмент"))
             {
                 Debug.Log("Building the bridge!");
-                animatorBridge.Play("BuildBridge");
-                animatorHint.Play("PressingE");
-                ShowHint(false);
-                canvasHint = null;
+
+                StartCoroutine(BuildingBridge(pl));
+                
+                // ShowHint(false);
+                // canvasHint = null;
                 return true;
             }
             else
@@ -43,5 +46,22 @@ public class Bridge : MonoBehaviour, IInteractable
     public void ShowHint(bool toShow) {
         if (canvasHint != null)
             canvasHint.enabled = toShow;
+    }
+    
+    private IEnumerator BuildingBridge(PlayerController pl)
+    {
+        animatorBridge.Play("BridgeBuild");
+        animatorHint.Play("PressingE");
+        
+        pl.TurnMovement(false);
+        pl.enabled = false;
+        
+        yield return new WaitForSeconds(2.5f);
+
+        pl.TurnMovement(true);
+        pl.enabled = true;
+        ShowHint(false);
+        canvasHint = null;
+        Destroy(this);
     }
 }
